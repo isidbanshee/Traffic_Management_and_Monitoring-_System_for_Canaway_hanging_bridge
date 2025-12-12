72ecf9842e9355b34534181285dd2ee28e62044d
Aliabo Dan Isidro <Danisidro.aliabo@yahoo.com>
2025-12-13 00:03:31 +0800

    Added Functionality for Device to set itself Online

--- Backend/controllers/device.controller.js
+++ Backend/controllers/device.controller.js
@@ -1,39 +1,57 @@
 import Device from "../models/device.model.js";
-import mongoose from "mongoose" ;
-
+import mongoose from "mongoose";
 
 export const registerNewDevice = async(req, res) =>{
-const deviceID  = req.body.deviceID;
-
-
+    const deviceID =  req.body.deviceID;
+    
     try{
         var newDevice = new Device();
         newDevice.deviceID = deviceID;
-       
+
         await newDevice.save();
         res.status(200).json({success: true, data: [newDevice]});
+    }catch(error){
+        res.status(500).json({success: false, message:"Server Error"});
+    }
+    
+    return res;
+}
 
+export const getDevices = async(req, res) =>{
+    try{
+        const devices = await Device.find({});
+        if(!devices){
+            res.status(500).json({success: false, message:"No device found!"});
+        }else{
+            res.status(200).json({success: true, data: devices});
+        }
     }catch(error){
         res.status(500).json({success: false, message:"Server Error"});
     }
 
     return res;
-
 }
+export const updateDevice = async(req, res)=>{}
 
-export const getDevices = async(req, res) =>{
+
+export const deviceOnline = async(req, res) =>{
+    const deviceID =  req.body.deviceID;
+    
     try{
-        const device = await Device.find({});
-        if(!device){
-            res.status(500).json({success: false, message:"No device found!"});
+        const result = await Device.find({deviceID});
+        if(!result){
+            res.status(500).json({success: false, message:"Device Not found!"});    
         }else{
-            res.status(200).json({success: true, data: device});
+            const device=result[0];
+
+            device.isOnline = true;
+            const updatedDevice = await Device.findByIdAndUpdate(device._id, device, {new: true});
+            res.status(200).json({success: true, data: [updatedDevice]});
         }
     }catch(error){
-        res.status(500).json({success: false, message:"Server Error!"});
+        console.log(error.message);
+        res.status(500).json({success: false, message:"Server Error"});
+    }
+    
+    return res;
 }
-return res;
-
-}
-
-export const updateDevice = async(req, res) =>{}
